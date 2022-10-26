bc22febbe8fbd884d57c3412800783d739b77242
Jaime Still <jpstill85@gmail.com>
2022-10-26 17:38:29 +0000

    initial

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double COFFEE_RATIO = 6.4;
+double WATER_RATIO = 100;
+
+for (double i = 1; i <= 10; i += 0.5) {
+    Console.WriteLine(i);
+    Console.WriteLine($"Coffee: {i * COFFEE_RATIO:#.#}g");
+    Console.WriteLine($"Water: {i * WATER_RATIO}g");
+
+    if (i < 10)
+        Console.WriteLine();
+}
